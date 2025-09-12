0b2b275080feacbf5111ee98ff9b175b0219af11
alextalijan <acatalijan@icloud.com>
2025-09-12 14:29:46 +0200

    feat: set up react router

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { Children } from 'react';
+import App from './App';
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'shop',
+        element: <Shop />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+    ],
+  },
+];
+
+export default routes;
