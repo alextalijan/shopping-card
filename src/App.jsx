af9cccaddc6138110fe6614a9dda53e96f7e99d7
alextalijan <acatalijan@icloud.com>
2025-09-12 15:19:03 +0200

    feat: write logic for fetching Fake Store API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Outlet, Link } from 'react-router-dom';
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [shopItems, setShopItems] = useState([]);
+  const [loadingShop, setLoadingShop] = useState(true);
+  const [shopError, setShopError] = useState(null);
 
   function addToCart(item) {
     setCart([...cart, item]);
@@ -12,6 +15,36 @@ function App() {
   function removeFromCart(id) {
     setCart(cart.filter((item) => item.id !== id));
   }
+
+  fetch('https://fakestoreapi.com/products/')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          'Failed to load items from the shop. Please try again by refreshing the page.',
+        );
+      }
+    })
+    .then((data) => {
+      const items = [];
+
+      for (let i = 0; i < 10; i += 1) {
+        items.push({
+          id: data[i].id,
+          title: data[i].title,
+          price: data[i].price,
+          description: data[i].description,
+          imageSrc: data[i].image,
+        });
+      }
+
+      setShopItems(items);
+    })
+    .catch((error) => {
+      setShopError(error.message);
+    })
+    .finally(() => {
+      setLoadingShop(false);
+    });
 
   return (
     <>
@@ -22,7 +55,15 @@ function App() {
           <span>({cart.length})</span> Cart
         </Link>
       </header>
-      <Outlet context={{ addToCart, removeFromCart }} />
+      <Outlet
+        context={{
+          addToCart,
+          removeFromCart,
+          shopItems,
+          loadingShop,
+          shopError,
+        }}
+      />
     </>
   );
 }
