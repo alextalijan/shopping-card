560fd088cf65f21b72531872a75a21d8f31a1edd
alextalijan <acatalijan@icloud.com>
2025-09-13 15:18:47 +0200

    style: add layout and styles to Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,21 @@
 import { useOutletContext } from 'react-router-dom';
 import CartItem from '../CartItem/CartItem';
+import styles from './Cart.module.css';
 
 function Cart() {
   const { cart, removeFromCart } = useOutletContext();
 
   return (
     <>
-      <h1>Your Cart</h1>
-      <div>
+      <h1 className={styles.h1}>Your Cart</h1>
+      <div className={styles['cart-list']}>
         {cart.length > 0 ? (
           cart.map((item) => {
             return (
               <CartItem
                 key={item.id}
                 title={item.title}
+                imageSrc={item.imageSrc}
                 price={item.price}
                 amount={item.amount}
                 onClick={() => removeFromCart(item.id)}
@@ -21,7 +23,9 @@ function Cart() {
             );
           })
         ) : (
-          <p>You have no items in the cart.</p>
+          <p className={styles['empty-cart-text']}>
+            You have no items in the cart.
+          </p>
         )}
       </div>
     </>
